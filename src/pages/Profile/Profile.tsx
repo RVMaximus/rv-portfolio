02a2af7d73bdd9429665ec2dab5cf3625dcba442
Raviraja <ravivasanth1996@gmail.com>
2025-08-18 23:56:24 +0530

    Profile page updated. Resume updated

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,58 @@
 import { Responsive, WidthProvider } from "react-grid-layout";
 import styles from "./Profile.module.scss";
+import { useEffect, useState } from "react";
+
+
+type expConfigType = {
+  id: string;
+  title: string;
+  org: string;
+  duration: string;
+  location: string;
+  roleDesc: string;
+  website: string;
+};
+
 
 export function Profile() {
+
+  const [expConfig, setExpConfig] = useState<expConfigType[]>([]);
+  const [projectsConfig, setProjectsConfig] = useState([]);
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        await Promise.all([
+          fetch("./experience.json")
+            .then((response) => {
+              if (!response.ok) throw new Error("Failed to fetch experience.json");
+              return response.json();
+            })
+            .then((data) => {
+              setExpConfig(data);
+            }),
+
+          fetch("./projects.json")
+            .then((response) => {
+              if (!response.ok) throw new Error("Failed to fetch projects.json");
+              return response.json();
+            })
+            .then((data) => {
+              setProjectsConfig(data);
+            })
+        ])
+      }
+      catch (e) {
+        console.error("Error in fetching data", e);
+      }
+    }
+    fetchProfileData();
+  }, [])
+
+  useEffect(() => {
+    console.log("expConfig in state", expConfig);
+  }, [expConfig])
+
   const ResponsiveGridLayout = WidthProvider(Responsive);
 
   const breakpoints = { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 };
@@ -9,18 +60,18 @@ export function Profile() {
   const layouts = {
     lg: [
       { i: "profile", x: 0, y: 0, w: 12, h: 1.1 },
-      { i: "experience", x: 0, y: 1.1, w: 6, h: 7.7 },
-      { i: "education", x: 6, y: 1.1, w: 6, h: 7.7 },
+      { i: "experience", x: 0, y: 1.1, w: 6, h: 5.5 },
+      { i: "projects", x: 6, y: 1.1, w: 6, h: 5.5 },
     ],
     md: [
       { i: "profile", x: 0, y: 0, w: 10, h: 1 },
-      { i: "experience", x: 0, y: 1, w: 5, h: 8.5 },
-      { i: "education", x: 5, y: 1, w: 5, h: 8.5 },
+      { i: "experience", x: 0, y: 1, w: 5, h: 6 },
+      { i: "projects", x: 5, y: 1, w: 5, h: 6 },
     ],
     xs: [
       { i: "profile", x: 0, y: 0, w: 6, h: 1.5 },
-      { i: "experience", x: 0, y: 1.5, w: 6, h: 8.5 },
-      { i: "education", x: 0, y: 10, w: 6, h: 3.5 },
+      { i: "experience", x: 0, y: 1.5, w: 6, h: 6.5 },
+      { i: "projects", x: 0, y: 10, w: 6, h: 3.5 },
     ],
   };
 
@@ -48,66 +99,31 @@ export function Profile() {
   const getExperience = () => {
     return (
       <>
-        <div className={styles.experienceContainer}>
-          <div className={styles.expTitle}>
-            <h3>Senior Software Engineer - I</h3>
-            <h5>April 2023 - Present</h5>
-          </div>
-          <h4>Brahmarsive INC</h4>
-          <p>Coimbatore</p>
-          <div className={styles.projectContainer}>
-            <h3>Projects:</h3>
-            <img src="./Brahmarsive.png" className={styles.projectImg} />
-            <p className={styles.projectDesc}>
-              I'm a software engineer specializing in full-stack and immersive
-              application development. I’ve worked on building modern web
-              interfaces, implemented interactive VR features using Babylon.js
-              such as lighting, sound, and hotspots, and developed dashboards
-              for data visualization. I’ve also contributed to advanced
-              healthcare simulations by enabling virtual dissection, skin
-              reflection, and step-by-step guided interactions using GUI and
-              audio. Additionally, I designed a flexible menu navigation system
-              to enhance user experience in immersive training modules.
-            </p>
-            <button
-              className={styles.redirection}
-              onClick={() => {
-                window.open("https://brahmarsive.com/", "_blank");
-              }}
-            >
-              <i className="fa-solid fa-arrow-up-right-from-square"></i>
-            </button>
-          </div>
-        </div>
-        <div className={styles.experienceContainer}>
-          <div className={styles.expTitle}>
-            <h3>Associate Software Engineer</h3>
-            <h5>Oct 2021 - March 2023</h5>
-          </div>
-          <h4>Nallas software solutions</h4>
-          <p>Coimbatore</p>
-          <div className={styles.projectContainer}>
-            <h3>Projects:</h3>
-            <img src="./Nallas.png" className={styles.projectImg} />
-            <p className={styles.projectDesc}>
-              Before becoming a full-time developer, I began my journey as an
-              intern where I focused on learning .NET Core. Later, I was
-              introduced to Sitecore, a powerful enterprise-level CMS, and
-              worked on a client project to build a complete website for a
-              company along with my team. This experience helped me understand
-              content-driven development, backend integration, and collaborative
-              delivery in a real-world project environment.
-            </p>
-            <button
-              className={styles.redirection}
-              onClick={() => {
-                window.open("https://nallas.com/", "_blank");
-              }}
-            >
-              <i className="fa-solid fa-arrow-up-right-from-square"></i>
-            </button>
-          </div>
-        </div>
+        {expConfig.map((experience: expConfigType) => {
+          return (
+            <div className={styles.experienceContainer}>
+              <div className={styles.expTitle}>
+                <h3>{experience.title}</h3>
+                <h5>{experience.duration}</h5>
+              </div>
+              <h4>{experience.org}</h4>
+              <p>{experience.location}</p>
+              <div className={styles.roleContainer}>
+                <p className={styles.roleDesc}>
+                  {experience.roleDesc}
+                </p>
+                <button
+                  className={styles.redirection}
+                  onClick={() => {
+                    window.open(`${experience.website}`, "_blank");
+                  }}
+                >
+                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                </button>
+              </div>
+            </div>
+          )
+        })}
       </>
     );
   };
@@ -123,25 +139,41 @@ export function Profile() {
           <h5>B.E EEE - 7.99 CGPA</h5>
           <h5>Rajapalayam</h5>
         </div>
-        <div className={styles.educationContainer}>
-          <div className={styles.eduDiv}>
-            <h3>Hindu Higher Secondary School</h3>
-            <h4>2012-2014</h4>
-          </div>
-          <h5>HSS - 92.6%</h5>
-          <h5>Watrap</h5>
-        </div>
-        <div className={styles.educationContainer}>
-          <div className={styles.eduDiv}>
-            <h3>Hindu Higher Secondary School</h3>
-            <h4>2010-2012</h4>
-          </div>
-          <h5>SSLC - 93.8%</h5>
-          <h5>Watrap</h5>
-        </div>
       </>
     );
   };
+
+  const getProjects = () => {
+    return (
+      <>
+        <div className={styles.projectsContainer}>
+          {
+            projectsConfig?.map((project: any) => {
+              return (
+                <>
+                  <div className={styles.projectDiv}>
+                    <h3 className={styles.projectTitle}>{project.title}</h3>
+                    <p className={styles.projectDescription}>{project.description}</p>
+                    <div className={styles.projectLinks}>
+                      <button
+                        className={styles.redirection}
+                        onClick={() => {
+                          window.open(`${project.link}`, "_blank");
+                        }}
+                      ><i className="fa-solid fa-arrow-up-right-from-square"></i></button>
+                      <span className={styles.projectGithub} onClick={() => {
+                        window.open(`${project.githubLink}`, "_blank");
+                      }}><i className="fa-brands fa-github fa-2x"></i></span>
+                    </div>
+                  </div>
+                </>
+              )
+            })
+          }
+        </div>
+      </>
+    )
+  }
 
   return (
     <>
@@ -164,13 +196,16 @@ export function Profile() {
         >
           <h3 className={styles.experienceHeader}>Work Experience</h3>
           {getExperience()}
-        </div>
-        <div
-          key="education"
-          className={`${styles.commonCardDiv} ${styles.educationDiv}`}
-        >
+
           <h3 className={styles.educationHeader}>Education</h3>
           {getEducation()}
+        </div>
+        <div
+          key="projects"
+          className={`${styles.commonCardDiv} ${styles.projectsDiv}`}
+        >
+          <h3 className={styles.projectsHeader}>Projects</h3>
+          {getProjects()}
         </div>
       </ResponsiveGridLayout>
     </>
